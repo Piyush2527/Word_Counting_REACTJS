7bfa38d849f66c306811ce828320fe9ff5171101
Piyush <datamain27@gmail.com>
2024-04-04 18:58:36 +0530

    adding Navbar component

---
+++ src/Components/Nav.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <>
+      <nav className="navbar bg-primary" data-bs-theme="dark">
+        <ul className="navbar-nav ">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Link
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled" aria-disabled="true">
+              Disabled
+            </a>
+          </li>
+        </ul>
+        <form className="d-flex" role="search">
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button className="btn btn-outline-success" type="submit">
+            Search
+          </button>
+        </form>
+      </nav>
+    </>
+  );
+}
+
+export default Nav;
